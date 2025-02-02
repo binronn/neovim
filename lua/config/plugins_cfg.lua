b97c51e2cfb9f13f2423c6513afc3a60414b1bc4
byron <barretguys@outlook.com>
2025-02-02 14:05:23 +0800

    终端快捷键修改

--- lua/config/plugins_cfg.lua
+++ lua/config/plugins_cfg.lua
@@ -308,7 +308,8 @@ function M.FTerm_init()
 			on_stderr = nil
 		}
 	)
-	nmap("<leader>tt", '<CMD>lua require("FTerm").toggle()<CR>')
+	vim.keymap.set('n', '<A-i>', '<CMD>lua require("FTerm").toggle()<CR>')
+	vim.keymap.set('t', '<A-i>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
 end
 
 ------------------------------------------------------------------------------------------
